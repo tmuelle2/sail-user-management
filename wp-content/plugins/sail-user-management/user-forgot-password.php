f9e2a1ece7f0ed2b2ef661fc4286a4dffc8871a2
Mules <aaronmueller64@gmail.com>
2021-07-12 15:52:55 -0400

    trying to get emails to send

--- wp-content/plugins/sail-user-management/user-forgot-password.php
+++ wp-content/plugins/sail-user-management/user-forgot-password.php
@@ -2,12 +2,12 @@
 
 $email = $_POST['email']
 $user = get_user_by( 'login', $email );
-if ( username_exists($email) && email_exists($email)) {
+if ( username_exists($email) && email_exists($email) && !is_wp_error( $user )) {
 
     $reset_key = get_password_reset_key($user);
     $user_login = $user->user_login;
-    $url = esc_url_raw( "https://sailhousingsolutions.org/change-password" . "?action=change_password&key=$reset_key&login=$user_login" );
-    $message = $url;
+    //$url = esc_url_raw( "https://sailhousingsolutions.org/change-password" . "?action=change_password&key=$reset_key&login=$user_login" );
+    $message = "hi"; //$url;
 
     wp_mail( $user_login, "SAIL Password Reset Link", $message );
     
