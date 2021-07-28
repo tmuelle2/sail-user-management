b20d0f880e6aa0b1560086299f4dcb9d4e3719ad
Dan <t.dan.muelle@gmail.com>
2021-07-27 21:25:15 -0700

    Fix debugging pring of date form population

--- wp-content/plugins/sail-user-management/sail-user-management.php
+++ wp-content/plugins/sail-user-management/sail-user-management.php
@@ -252,7 +252,8 @@ function populate_select($dom_select, $option) {
 }
 
 function populate_element($dom_element, $value) {
-  dom_named_node_map_to_string($dom_element);
+  print_r($dom_element, true);
+  dom_named_node_map_to_string($dom_element[0]);
   if (count($dom_element) > 1) {
     return;
   }
