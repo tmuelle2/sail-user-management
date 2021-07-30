1bd16e28a84b2266774ea76524318a700d568ab4
Dan <t.dan.muelle@gmail.com>
2021-07-30 16:45:29 -0700

    Fix update where clause and cleanup logging

--- wp-content/plugins/sail-user-management/sail-user-management.php
+++ wp-content/plugins/sail-user-management/sail-user-management.php
@@ -229,14 +229,7 @@ function populate_input($dom_input, $value) {
       }
     }
   } elseif ($count == 1) { 
-    if ($dom_input[0]->attributes->getNamedItem('type')->nodeValue == 'date') {
-      $attrs = dom_named_node_map_to_string($dom_input[0]->attributes);
-      error_log("There are $count elements, the first element's attributes are: $attrs");
-      error_log($value);
-      $dom_element[0]->nodeValue = $value;
-    } else {
-      $dom_input[0]->setAttribute('value', $value);
-    }
+    $dom_input[0]->setAttribute('value', $value);
   } else {
     $attrs = dom_named_node_map_to_string($dom_input[0]->attributes);
     error_log("Unsupported input element(s). There are $count elements, the first element's attributes are: $attrs");
--- wp-content/plugins/sail-user-management/user-update.php
+++ wp-content/plugins/sail-user-management/user-update.php
@@ -33,8 +33,9 @@ if (is_user_logged_in()) {
         $data['profilePicture'] = $cur_user->profilePicture;
     }
 
+    error_log($cur_user->userId);
     // Update SAIL users db table
-    $wpdb->update('sail_users', $data, array('userId', $cur_user->userId), $formats);
+    $wpdb->update('sail_users', $data, array('userId' => $cur_user->userId), $formats);
 
     // Success redirect
     nocache_headers();
