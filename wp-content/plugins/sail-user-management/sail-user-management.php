af24f817b3b1b8d53a635b0bc48a9313ddbd031c
Mules <aaronmueller64@gmail.com>
2021-09-20 23:37:13 -0400

    boom

--- wp-content/plugins/sail-user-management/sail-user-management.php
+++ wp-content/plugins/sail-user-management/sail-user-management.php
@@ -181,7 +181,8 @@ function fc_update_shortcode($atts = [], $content = null, $tag = '' ) {
       error_log("[fc_update_shortcode] Now printing debug vars: ");
       error_log(print_r($fc_member, true));
       $html = parse_html(get_sail_page($PAGES_DIR . 'fc-profile-update.html'));
-      populate_form_elements($html, $FC_DB_FIELDS, $fc_member);      
+      populate_form_elements($html, $FC_DB_FIELDS, $fc_member);
+      $html->saveHTML();      
     
       $firstNameAndLastInitial = $sail_user->firstName . " " . $sail_user->lastName[0] . ".";
       $initials = strtoupper($sail_user->firstName[0]) . "." . strtoupper($sail_user->lastName[0]) . ".";
@@ -368,8 +369,8 @@ function get_fc_member() {
 
     $result = $wpdb->get_row($query);
     error_log("[get_fc_member] Now printing debug vars: ");
-    print_r($query, true);
-    print_r($result, true);
+    error_log(print_r($query, true));
+    error_log(print_r($result, true));
     return $result;
 }
 
