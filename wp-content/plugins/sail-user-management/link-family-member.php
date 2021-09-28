4d119854d15e0da87d40e0fe65d3008e7ba522ae
Dan <t.dan.muelle@gmail.com>
2021-09-27 21:00:37 -0700

    Change verification key to link key

--- wp-content/plugins/sail-user-management/link-family-member.php
+++ wp-content/plugins/sail-user-management/link-family-member.php
@@ -5,7 +5,7 @@ global $wp;
 if (strpos($wp->request, 'link-family-member') !== false) {
     error_log('Attempting to link family member with ' . $_GET['email'] . ' with code ' . $_GET['family_linking_key']);
     // Ensure query string parameters exist 
-    if (isset($_GET['verification_key']) && isset($_GET['email']) 
+    if (isset($_GET['family_linking_key']) && isset($_GET['email']) 
         && email_exists($_GET['email'])) {
 
         $family_linking_key = $_GET['family_linking_key'];
