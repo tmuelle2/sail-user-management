df4ca4c4a1dcdfec15a56876ca928ded7cefd5ea
Dan <t.dan.muelle@gmail.com>
2021-10-12 17:48:13 -0700

    Move list id to env var and fix it

--- wp-content/plugins/sail-user-management/mail-chimp.php
+++ wp-content/plugins/sail-user-management/mail-chimp.php
@@ -7,12 +7,13 @@ use GuzzleHttp\Exception\ClientException;
 class MailChimpSailNewsletterClient {
 
     private $client;
-    private static $listId = '1304042';
-    
+    private $listId;     
+
     public function __construct()  {
         error_log('Loading ClassAutoloader...');
         require('/home2/sailhou1/public_html/wp-content/plugins/sail-user-management/ClassAutoloader.php');
         spl_autoload_register('ClassAutoloader::autoload');
+        $listId = getenv('MAIL_CHIP_LIST_ID') ?: 'MAIL_CHIP_LIST_ID';
         $this->client = new ApiClient();
         $this->client->setConfig([
             'apiKey' => getenv('MAIL_CHIP_API_KEY') ?: 'MAIL_CHIP_API_KEY',
@@ -30,7 +31,7 @@ class MailChimpSailNewsletterClient {
 
     public function status($email) {
         try {
-            $response = $this->client->lists->getListMember(self::$listId, md5(strtolower($email)));
+            $response = $this->client->lists->getListMember($this->listId, md5(strtolower($email)));
             return $response->status;
         } catch (ApiException | ClientException $e) {
             error_log($e->getMessage());
@@ -41,7 +42,7 @@ class MailChimpSailNewsletterClient {
     private function update_list($email, $status) {
         try {
             error_log('Attempting to update ' . $email . ' newsletter subscription to ' . $status);
-            $response = $this->client->lists->updateListMember(self::$listId, md5(strtolower($email)), ['status' => $status]);
+            $response = $this->client->lists->updateListMember($this->listId, md5(strtolower($email)), ['status' => $status]);
             error_log(print_r($response, true));
             return $response;
         } catch (ApiException | ClientException $e) {
