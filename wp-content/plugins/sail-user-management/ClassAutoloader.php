e98c3cedcc2e559e9f2eb65c5f5f145889615756
Dan <t.dan.muelle@gmail.com>
2021-10-12 17:32:53 -0700

    Revert ClassAutoloader changes in favor of mail chimp lib monkey patch

--- wp-content/plugins/sail-user-management/ClassAutoloader.php
+++ wp-content/plugins/sail-user-management/ClassAutoloader.php
@@ -8,7 +8,7 @@ class ClassAutoloader {
             return;
         }
 
-        error_log('Initializing library paths...');
+        //error_log('Initializing library paths...');
         $HOME_DIR = '/home2/sailhou1/public_html/wp-content/plugins/sail-user-management/';
         $libPaths = glob($HOME_DIR . 'paypalhttp_php-1.0.0/lib/PayPalHttp/Serializer/*.php');
         $libPaths = array_merge($libPaths, glob($HOME_DIR . 'paypalhttp_php-1.0.0/lib/PayPalHttp/*.php'));
@@ -24,35 +24,24 @@ class ClassAutoloader {
         $libPaths = array_merge($libPaths, glob($HOME_DIR . 'promises-1.5.0/src/*.php'));
         $libPaths = array_merge($libPaths, glob($HOME_DIR . 'http-client-1.0.1/src/*.php'));
         $libPaths = array_merge($libPaths, glob($HOME_DIR . 'http-message-1.0/src/*.php'));
-        
+
         $namespaceRegex =  '/^namespace (.*);$/m';
-        $functionRegex =  '/^function (.*)\(.*$/m';
         foreach ($libPaths as $path) {
             $split = explode('/', $path);
             $fileNameWithExt = end($split);
             $justFileName = basename($fileNameWithExt, '.php');
-            $fileContents = file_get_contents($path);
-            // This loads full namespaced classes assuming file name and class name matches 
-            if (preg_match($namespaceRegex, $fileContents, $namespaceMatches)) {
+            if (preg_match($namespaceRegex, file_get_contents($path), $namespaceMatches)) {
                 self::$classPathMap[$namespaceMatches[1] . '\\' . $justFileName] = $path;
-            // This hack loads some GuzzleHttp functions 
-            } else if (self::endsWith($path, 'functions.php')) {
-                $preg_match($functionRegex, $fileContents, $functionMatches);
-                // Every other match will be the group with the function name
-                for ($i = 1; $i < count($functionMatches); $i += 2) {
-                    self::$classPathMap[$functionMatches[i]] = $path;
-                }
-            // This loads naked classes without namespaces assuming file name and class name matches 
             } else {
                 self::$classPathMap[$justFileName] = $path;
             }
         }
-        error_log('Class path map: ' . print_r(self::$classPathMap, true));
+        //error_log('Class path map: ' . print_r(self::$classPathMap, true));
     }
 
     public static function autoload($className) {
         self::init(); 
-        error_log('ClassAutoloader loading: ' . $className);
+        //error_log('ClassAutoloader loading: ' . $className);
         if (isset(self::$classPathMap[$className])) {
             require_once(self::$classPathMap[$className]);
         } else {
@@ -62,15 +51,5 @@ class ClassAutoloader {
                 require_once(self::$classPathMap[$justClassName]);
             }
         }
-    }
-
-    // This can be deleted if Bluhost migrates to PHP 8
-    // https://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
-    private static function endsWith( $haystack, $needle ) {
-        $length = strlen( $needle );
-        if( !$length ) {
-            return true;
-        }
-        return substr( $haystack, -$length ) === $needle;
     }
 }
