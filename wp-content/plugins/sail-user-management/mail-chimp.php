4390f8ac96972c0771bb37feecb606feda6348e9
Dan <t.dan.muelle@gmail.com>
2021-10-12 17:49:11 -0700

    Fix list reference in constructor

--- wp-content/plugins/sail-user-management/mail-chimp.php
+++ wp-content/plugins/sail-user-management/mail-chimp.php
@@ -13,7 +13,7 @@ class MailChimpSailNewsletterClient {
         error_log('Loading ClassAutoloader...');
         require('/home2/sailhou1/public_html/wp-content/plugins/sail-user-management/ClassAutoloader.php');
         spl_autoload_register('ClassAutoloader::autoload');
-        $listId = getenv('MAIL_CHIP_LIST_ID') ?: 'MAIL_CHIP_LIST_ID';
+        $this->listId = getenv('MAIL_CHIP_LIST_ID') ?: 'MAIL_CHIP_LIST_ID';
         $this->client = new ApiClient();
         $this->client->setConfig([
             'apiKey' => getenv('MAIL_CHIP_API_KEY') ?: 'MAIL_CHIP_API_KEY',
