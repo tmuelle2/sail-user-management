a94ff1041693acb3f18af5eabd456cdc9fedcd52
Dan Mueller <t.dan.mueller@gmail.com>
2022-07-18 18:56:29 -0700

    Add form-base for shared form submission logic

---
+++ wp-content/plugins/sail-user-management/pages/templates/form-base.php
@@ -0,0 +1,98 @@
+<?php use Sail\Constants; ?>
+<html>
+<script id='form-base'>
+    window.onload = function () {
+        <?php
+        foreach ($formActions as $action) {
+            echo "makeFormRestSubmit('" . $action['id'] . "', '" . Constants::FORM_REST_PREFIX . $action['action'] . "');";
+        }
+        ?>
+    };
+
+    const BASE64_PREFIX = "data:*/*;base64,";
+    const buttons = [...document.getElementsByTagName('button')];
+    const loadingSpinner = document.createElement('img');
+    loadingSpinner.setAttribute('src', <?php echo '"' . Constants::IMAGES_ROUTE . 'loading-spinner.gif"' ?>);
+    loadingSpinner.setAttribute('alt', 'Loading');
+
+    function makeFormRestSubmit(formId, apiRoute) {
+        const form = document.getElementById(formId);
+        const button = buttons.filter(b => b.getAttribute('form') == formId)[0];
+        form.onsubmit = (function(event) {
+            buttonDisable(button);
+            return formRestSubmit(form, button, apiRoute, event)
+        });
+    }
+
+    function buttonDisable(button) {
+        button.disabled = true;
+        button.append(loadingSpinner);
+    }
+
+    function buttonEnable(button) {
+        button.disabled = false;
+        if (button.lastElementChild.tagName == 'img') {
+            button.lastElementChild.remove();
+        }
+    }
+
+    function formRestSubmit(form, button, apiRoute, event) {
+        const fileInput = document.querySelector('input[type=file]');
+        const formData = new FormData(form);
+        let reader = null;
+
+        for(var kvPair of formData.entries()) {
+            if (Array.isArray(formData.getAll(kvPair[0])) && formData.getAll(kvPair[0]).length > 1) {
+                formData.set(kvPair[0], formData.getAll(kvPair[0]).join('|'));
+            } else if(fileInput && kvPair[0] === fileInput.id) {
+                reader = new FileReader();
+            }
+        }
+
+        if (reader) {
+            reader.addEventListener("load", function () {
+                let fileContents = reader.result;
+                if (fileContents.startsWith(BASE64_PREFIX)) {
+                    fileContents = fileContents.slice(BASE64_PREFIX.length);
+                }
+                formData.set(fileInput.id, fileContents);
+                postData(apiRoute, formData, button);
+            }, false);
+            reader.readAsDataURL(fileInput.files[0]);
+        } else {
+            postData(apiRoute, formData, button);
+        };
+
+        return false;
+    }
+
+    function postData(apiRoute, formData, button) {
+        const formHeaders = new Headers({
+            'Content-Type': 'application/json',
+            'x-wp-nonce': SAIL.nonce
+        });
+
+        fetch(apiRoute, {
+                method: 'POST',
+                headers: formHeaders,
+                body: JSON.stringify(Object.fromEntries(formData))
+            })
+            .then(response => {
+                if (response.headers.get('Location')) {
+                    location.href = response.headers.get('Location');
+                }
+                if (response.redirected) {
+                    location.href = response.url;
+                }
+                buttonEnable(button);
+                return response.json();
+            })
+            .catch(error => {
+                buttonEnable(button);
+                console.error(error)
+                return false;
+            });
+        return false;
+    }
+</script>
+</html>
