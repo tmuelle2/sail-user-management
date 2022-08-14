42efd2ffb719bd99d381cf43bd3c191f12e09446
armuell <aaronmueller64@gmail.com>
2022-08-13 23:07:34 -0400

    idk

--- wp-content/plugins/sail-user-management/api/SailApi.php
+++ wp-content/plugins/sail-user-management/api/SailApi.php
@@ -18,8 +18,8 @@ abstract class SailApi
   {
     register_rest_route($this->getRoutePrefix(), $this->getApiRoute(), array(
       'methods' => $this->getMethod(),
-      'callback' => array($this, 'callback'),
-      'permission_callback' => array($this, 'permissionCallback'),
+      'callback' => 'callback',
+      'permission_callback' => 'permissionCallback',
     ));
   }
 
