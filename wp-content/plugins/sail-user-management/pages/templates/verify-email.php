0b221f5f6781c55eb217e46f6492672e5df68e5b
armuell <aaronmueller64@gmail.com>
2022-09-22 15:17:21 -0400

    typo

--- wp-content/plugins/sail-user-management/pages/templates/verify-email.php
+++ wp-content/plugins/sail-user-management/pages/templates/verify-email.php
@@ -2,7 +2,7 @@
     <summary>Please Verify Your Email</summary>
     <div>
         <p>If you have already gotten a verification email, clicking the link will verify your email and this section will not appear on your profile any more.</p>
-        <p>If you can't find your verrification email, click the button below to send a new one.</p>
+        <p>If you can't find your verification email, click the button below to send a new one.</p>
         <form accept-charset="UTF-8" autocomplete="on" action='verify-email'>
             <input type="hidden" name="action" value="sail_user_reverify_email">
         </form>
